f530272d05c7c9b1f0929f6b68fb49d05a4c61c8
BailiMohamedAli <bailimohamedali@gmail.com>
2021-01-08 20:55:21 +0100

    baili editing signup for aziz to connect backend

--- fooapp/src/views/mains/Mainfeed.vue
+++ fooapp/src/views/mains/Mainfeed.vue
@@ -1,15 +1,51 @@
 <template>
     <div>
-
+        <div class="con-type-example">
+                <vs-navbar :type="type" v-model="activeItem" class="nabarx spacing-added-to-nav">
+                <div slot="title">
+                <vs-navbar-title>
+                Logo under construction
+                </vs-navbar-title>
+                </div>
+                    <vs-navbar-item class="spacing-navbar-element">
+                        {{userstatus}}
+                    </vs-navbar-item>
+                    <vs-navbar-item class="spacing-navbar-element">
+                        {{username}}
+                    </vs-navbar-item>
+                    <vs-navbar-item class="spacing-navbar-element" >
+                        <span @click="logout" >Logout</span>
+                    </vs-navbar-item>
+                </vs-navbar>
+        </div>
+        <h1>this is the mainfeed page</h1>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "Mainfeed",
+        data: () => ({
+            popupActivo: false,
+            userstatus: "user",
+            username: "Garbouj",
+        }),
     }
 </script>
 
 <style scoped>
-
+    .spacing-added-to-nav{
+        padding: 1rem 2.4rem;
+        background: #fff;
+    }
+    .spacing-navbar-element{
+        margin: 0 0.6rem;
+    }
+    .button-nav-spacing{
+        width: 7.6rem;
+        border-radius: 25px;
+    }
+    span{
+        cursor: pointer;
+    }
 </style>
--- fooapp/src/views/sign-in-up/Signup.vue
+++ fooapp/src/views/sign-in-up/Signup.vue
@@ -1,87 +1,90 @@
 <template lang="html">
   <div id="pop-btn">
-    <vs-button @click="popupActivo = true" color="danger" type="border"
-      >Sign Up</vs-button
+    <vs-button @click="popupActivo = true" color="primary" type="filled" class="button-nav-spacing">Sign Up</vs-button
     >
     <vs-popup
       class="holamundo"
       title="Create Your Account !"
-      :active.sync="popupActivo"
-    >
-      <p>Sign Up</p>
+      :active.sync="popupActivo">
       <vs-input
         label="Username"
         class="inputx"
         placeholder="Placeholder"
-        v-model="fname"
-      />
+        v-model="fname"/>
       <vs-input
         label="email"
         class="inputx"
         placeholder="email"
-        v-model="email"
-      />
+        v-model="email"/>
       <vs-input
         type="password"
         label="Password"
-        class="inputx ml-3"
+        class="inputx"
         placeholder="Password"
-        v-model="password"
-      />
-      <div class="centerx">
-        <vs-button
-          @click="
-            $vs.notify({
-              title: 'Support',
-              text: 'thank you for subscribing with us',
-              color: 'danger',
-            })
-          "
-          color="danger"
-          type="danger"
-        >
-          <vs-button @click="openLoadingColor" type="gradient" color="danger"
-            >Danger</vs-button
-          >
-        </vs-button>
-      </div>
+        v-model="password"/>
+            <span  @click="
+                $vs.notify({
+                title: 'Support',
+                text: 'thank you for subscribing with us',
+                color: 'primary',
+                })">
+                <vs-button @click="openLoadingColor" type="filled" color="primary" class="button-inside-sign-spacing">Register</vs-button>
+            </span>
     </vs-popup>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      colorLoading: "#7d0c3f",
-      popupActivo: false,
-      fname: "",
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    openLoadingColor() {
-      this.$vs.loading({ color: this.colorLoading });
-      setTimeout(() => {
-        this.$vs.loading.close();
-      }, 2000);
+    name: "Signup",
+    data() {
+        return {
+        colorLoading: "#7d0c3f",
+        popupActivo: false,
+        fname: "",
+        email: "",
+        password: "",
+        }
     },
-    randomIcon() {
-      function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      }
-      let color = `rgb(${getRandomInt(0, 255)},${getRandomInt(
-        0,
-        255
-      )},${getRandomInt(0, 255)})`;
-      this.$vs.notify({
-        title: "Icon mail",
-        text: "Lorem ipsum dolor sit amet, consectetur",
-        color: color,
-        icon: "verified_user",
-      });
+    methods: {
+        openLoadingColor() {
+        this.$vs.loading({ color: this.colorLoading });
+        setTimeout(() => {
+            this.$vs.loading.close();
+        }, 2000);
+        },
+        randomIcon() {
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+        let color = `rgb(${getRandomInt(0, 255)},${getRandomInt(
+            0,
+            255
+        )},${getRandomInt(0, 255)})`;
+        this.$vs.notify({
+            title: "Icon mail",
+            text: "Lorem ipsum dolor sit amet, consectetur",
+            color: color,
+            icon: "verified_user",
+        });
     },
   },
 };
 </script>
+<style scoped>
+    .holamundo{
+        text-align: center;
+    }
+    .inputx{
+        margin: 1rem auto;
+    }
+    .button-nav-spacing{
+        width: 7.6rem;
+        border-radius: 25px;
+    }
+    .button-inside-sign-spacing{
+            width: 7.6rem;
+            border-radius: 25px;
+            margin: 1.2rem 0 1rem 0;
+    }
+</style>
